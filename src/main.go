3535c5d6225f2861242947b2c117d9c2b1090fa2
Eduard Khiaev <eddietheone1984@gmail.com>
2021-03-31 05:22:06 +0300

    static files

--- src/main.go
+++ src/main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	// app.Static("/", "./public")
+	// => http://localhost:80/js/script.js
+	// => http://localhost:80/css/style.css
 
-	app.Listen(":80")
+	// app.Static("/prefix", "./public")
+	// => http://localhost:80/prefix/js/script.js
+	// => http://localhost:80/prefix/css/style.css
+
+	app.Static("*", "./public/index.html")
+	// => http://localhost:80/any/path/shows/index/html
+
+	log.Fatal(app.Listen(":80"))
 }
