560a1c85d721f0f989a61495fc7a204e728055b4
Eduard Khiaev <eddietheone1984@gmail.com>
2021-04-07 06:43:27 +0300

    123

--- src/main.go
+++ src/main.go
@@ -4,21 +4,17 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+	// app.Use(cors.New())
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins: "*",
+	// 	AllowHeaders: "Origin, Content-Type, Accept",
+	// }))
 
-	// app.Static("/", "./public")
-	// => http://localhost:80/js/script.js
-	// => http://localhost:80/css/style.css
-
-	// app.Static("/prefix", "./public")
-	// => http://localhost:80/prefix/js/script.js
-	// => http://localhost:80/prefix/css/style.css
-
-	app.Static("*", "./public/index.html")
-	// => http://localhost:80/any/path/shows/index/html
-
+	app.Static("/", "./public")
 	log.Fatal(app.Listen(":80"))
 }
