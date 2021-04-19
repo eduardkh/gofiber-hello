a6dc94d265bdff73f9aa2d3c0f10668d54926841
Eduard Khiaev <eddietheone1984@gmail.com>
2021-04-19 10:42:32 +0300

    add logger and cors

--- src/go.mod
+++ src/go.mod
@@ -3,11 +3,6 @@ module github.com/eduardkh/gofiber-hello
 go 1.16
 
 require (
-	github.com/cosmtrek/air v1.21.2 // indirect
-	github.com/creack/pty v1.1.11 // indirect
-	github.com/fatih/color v1.10.0 // indirect
-	github.com/gofiber/fiber/v2 v2.7.1 // indirect
-	github.com/imdario/mergo v0.3.12 // indirect
-	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/gofiber/fiber/v2 v2.7.1
 	golang.org/x/sys v0.0.0-20210326220804-49726bf1d181 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -4,17 +4,21 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	_ "github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
-	// app.Use(cors.New())
-	// app.Use(cors.New(cors.Config{
-	// 	AllowOrigins: "*",
-	// 	AllowHeaders: "Origin, Content-Type, Accept",
-	// }))
-
+	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+	app.Use(logger.New())
+	app.Get("/suka", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 	app.Static("/", "./public")
 	log.Fatal(app.Listen(":80"))
 }
